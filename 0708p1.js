f345829410d63e7c604a4bdf47b3534bab3c89ec
Dennis <dlex351279@gmail.com>
2021-07-08 23:24:21 +0800

    新增0708上課內容

---
+++ 0708p1.js
@@ -0,0 +1,16 @@
+console.log('hi'); console.log('aaaaaaaa');
+
+console
+.log('bbbbbbb')
+
+
+console.log(a)
+var a = 'hi'
+var c = null
+console.log(c)
+
+
+console.log(b)
+
+
+
---
+++ 0708p2.js
@@ -0,0 +1,24 @@
+// let a;
+// console.log(a)
+
+
+let age = 22
+let citizen = true
+
+if (456) {
+	if (citizen) {
+		console.log('可以投票')
+	} else {
+		console.log('不能投票')
+	}
+} else {
+	console.log('不能投票')
+}
+
+
+if (age >= 20 && !citizen) {
+	console.log('可以投票')
+} else {
+	console.log('不能投票')
+}	
+
