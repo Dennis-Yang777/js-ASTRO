d7c5c0fc48bf2090ee6c173e9b9aafac7c73e698
Dennis <dlex351279@gmail.com>
2021-08-01 22:33:49 +0800

    計算機完整版

--- 計算機/caculator.js
+++ 計算機/caculator.js
@@ -8,81 +8,103 @@ document.addEventListener('DOMContentLoaded', () => {
 	let total = 0
 
 	const result = {
-		number : '',
-		number2 : '',
+		displayNumber : '',
+		savedNumber : '',
 		operator : '',
 		toInt() {
-			this.number = parseFloat(this.number)
-			this.number2 = parseFloat(this.number2)
-			return parseFloat(this.number) 
+			this.displayNumber = parseFloat(this.displayNumber)
+			this.savedNumber = parseFloat(this.savedNumber)
 		},
 		toString() {
-			this.number = this.number.toString()
-			// this.number2 = this.number2.toString()
-			return this.number.toString() 
+			this.displayNumber = this.displayNumber.toString()
+			this.savedNumber = this.savedNumber.toString()
 		}
 	}
 	
 
 	function reset() {
 		display.innerText = '0'
-		result.number = ''
+		result.displayNumber = ''
+		result.savedNumber = ''
+		result.operator = ''
+	}
+
+	function pack () {
+		result.savedNumber = total
+		display.innerText = total
+		result.displayNumber = ''
+		result.toString()
 	}
 
 	function operation () {
-		result.number = total
-		display.innerText = total
-		result.number2 = ''
-		result.toString()
+		switch (result.operator) {
+			case '+':
+				total = result.savedNumber + result.displayNumber
+				pack()
+				break
+			case '-':
+				total = result.savedNumber - result.displayNumber
+				pack()
+				break
+			case '*':
+				total = result.savedNumber * result.displayNumber
+				pack()
+				break
+			case '/':
+				total = result.savedNumber / result.displayNumber
+				pack()
+				break
+			default :
+				return
+		} 
 	}
 
 	numberGroup.forEach((ele, index) => {
 		numberGroup[index].addEventListener('click', () => {
-			let eachNumber = numberGroup[index].innerText
-			result.number = result.number.concat(eachNumber)
-			if (result.number === '00' && eachNumber === '0') {
-				result.number = '0'
-			} else if (result.number === '0') {
-				result.number = '0'
+			let whichNumber = numberGroup[index].innerText
+			let change = result.displayNumber.concat(whichNumber)
+			
+			if (result.displayNumber === '0' && whichNumber === '0') {
+				result.displayNumber = '0'
+			} else if(change.length === 2 && whichNumber !== '0' && change.indexOf(0) === 0) {
+				result.displayNumber = whichNumber
+				display.innerText = result.displayNumber
+			} else if (whichNumber === '.' && /[.]+/.test(result.displayNumber)) {
+				return
 			} else {
-				display.innerText = result.number
+				result.displayNumber = change
+				display.innerText = result.displayNumber
 			}
 		})
 	})
 
 	operator.forEach((ele, index) => {
 		operator[index].addEventListener('click', () => {
-			let whichOperator = operator[index].innerText
+			let whichOperator = operator[index].id
 			if (operator[index].innerText === '=') {
-				if (result.number && result.number2) {
+				if (result.displayNumber && result.savedNumber) {
 					result.toInt()
-					switch (result.operator) {
-						case '+':
-							total = result.number2 + result.number
-							operation()
-							break
-						case '-':
-							total = result.number2 - result.number
-							operation()
-							break
-						case '*':
-							total = result.number2 * result.number
-							operation()
-							break
-						case '/':
-							total = result.number2 / result.number
-							operation()
-							break
-						default :
-							return
-					} 
+					operation()
+					result.operator = whichOperator
 				} else {
-					true
+					return
 				}
 			} else {
-				result.number2 = result.number
-				result.operator = whichOperator
-				result.number = ''
+				if (result.operator.length === 0) {
+					result.savedNumber = result.displayNumber
+					result.operator = whichOperator
+					result.displayNumber = ''
+				} else if (result.operator === '=') {
+					result.operator = whichOperator
+				} else {
+					if (result.displayNumber && result.savedNumber) {
+						result.toInt()
+						operation()
+						result.operator = whichOperator
+					} else{
+						return
+					}
+				}
 			}
 		})
 	})
@@ -92,19 +114,34 @@ document.addEventListener('DOMContentLoaded', () => {
 	})
 
 	negative.addEventListener('click', () => {
-		let change = result.toInt() * -1
-		display.innerText = change
-		result.number = change
-		result.toString()
+		if (result.displayNumber) {
+			let change = result.displayNumber * -1
+			display.innerText = change
+			result.displayNumber = change
+			result.toString()
+		} else if (result.savedNumber) {
+			let change = result.savedNumber * -1
+			display.innerText = change
+			result.savedNumber = change
+			result.toString()
+		} else {
+			return
+		}
 	})
 
 	percent.addEventListener('click', () => {
-		let change = result.toInt() / 100
-		display.innerText = change
-		result.number = change
-		result.toString()
+		if (result.displayNumber) {
+			let change = result.displayNumber / 100
+			display.innerText = change
+			result.displayNumber = change
+			result.toString()
+		} else if (result.savedNumber) {
+			let change = result.savedNumber / 100
+			display.innerText = change
+			result.savedNumber = change
+			result.toString()
+		} else {
+			return
+		}
 	})
-
-
-
 })
